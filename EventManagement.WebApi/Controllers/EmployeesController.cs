a06e47a6a1d682aa9a05463ea02f18cb8009076b
Akash Paul <paulakash36@gmail.com>
2023-01-05 04:54:24 +0530

    Employee - Controller and action methods for CRUD operations

---
+++ EventManagement.WebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,82 @@
+﻿using EventManagement.Dal;
+using EventManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagement.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ICommonRepository<Employee> _repository;
+        public EmployeesController(ICommonRepository<Employee> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            var employees = _repository.GetAll();
+            if (employees.Count == 0)
+                return NotFound();
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<Employee>> GetDetails(int id)
+        {
+            var employee = _repository.GetDetails(id);
+            return employee == null ? NotFound() : Ok(employee);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult Create(Employee employee)
+        {
+            _repository.Insert(employee);
+            var result = _repository.SaveChanges();
+            if (result > 0)
+                return CreatedAtAction("GetDetails", new { id = employee.EmployeeId }, employee);
+            return BadRequest();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public ActionResult Update(Employee employee)
+        {
+            _repository.Update(employee);
+            var result = _repository.SaveChanges();
+            if (result > 0)
+                return NoContent();
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public ActionResult<Employee> Delete(int id)
+        {
+            var employee = _repository.GetDetails(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _repository.Delete(employee);
+                _repository.SaveChanges();
+                return NoContent();
+            }
+        }
+
+
+    }
+}
---
+++ EventManagement.WebApi/Program.cs
@@ -0,0 +1,43 @@
+using EventManagement.Dal;
+using EventManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManagement.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<EventManagementDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MsSqlConStr")));
+
+            builder.Services.AddTransient<ICommonRepository<Employee>, CommonRepository<Employee>>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
