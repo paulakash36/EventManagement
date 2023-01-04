aa5dd2288221b00929d4f49c2c2e44cf48e11746
Akash Paul <paulakash36@gmail.com>
2023-01-05 02:59:10 +0530

    Adding Data Migration using CodeFirst Approach

---
+++ EventManagement.Dal/EventManagementDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using EventManagement.Models;
+ 
+namespace EventManagement.Dal
+{
+    public class EventManagementDbContext : DbContext
+    {
+        public EventManagementDbContext()
+        {
+
+        }
+
+        public EventManagementDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Data Source = localhost; Initial Catalog = EventMgtDb; Integrated Security = true; TrustServerCertificate=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee() { EmployeeId = 1, EmployeeName = "AK PAUL", Address = "Kenchenhalli", City = "Bangalore", Country = "India", Zipcode = "560098", Phone = "+91 1234567890", Email = "paulak@gmail.com", Skillsets = ".NET Developer", Avatar = "/images/ak.jpg" },
+                new Employee() { EmployeeId = 2, EmployeeName = "K PAUL", Address = "Narayanpur", City = "Balurghat", Country = "India", Zipcode = "733101", Phone = "+91 1235567890", Email = "paulk@gmail.com", Skillsets = "People Management", Avatar = "/images/k.jpg" },
+                new Employee() { EmployeeId = 3, EmployeeName = "AB PAUL", Address = "Kenchenhalli", City = "Bangalore", Country = "India", Zipcode = "560098", Phone = "+91 12333567890", Email = "paulab@gmail.com", Skillsets = "Trainer and Consultant", Avatar = "/images/ab.jpg" }
+                );
+
+            modelBuilder.Entity<Role>().HasData(
+                new Role() { RoleId = 1, RoleName = "Employee", RoleDescription = "Employee of Organisation" },
+                new Role() { RoleId = 2, RoleName = "HR", RoleDescription = "HR of Organisation" }
+                );
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ EventManagement.Dal/Migrations/20230104211856_CreateNewDatabase.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EventManagement.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateNewDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Zipcode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Skillsets = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Avatar = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Eventid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventCode = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    EventName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Fees = table.Column<decimal>(type: "Decimal(18,2)", nullable: false),
+                    SeatsFilled = table.Column<int>(type: "int", nullable: false),
+                    Logo = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Eventid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    RoleDescription = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RoleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Users_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "RoleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "Address", "Avatar", "City", "Country", "Email", "EmployeeName", "Phone", "Skillsets", "Zipcode" },
+                values: new object[,]
+                {
+                    { 1, "Kenchenhalli", "/images/ak.jpg", "Bangalore", "India", "paulak@gmail.com", "AK PAUL", "+91 1234567890", ".NET Developer", "560098" },
+                    { 2, "Narayanpur", "/images/k.jpg", "Balurghat", "India", "paulk@gmail.com", "K PAUL", "+91 1235567890", "People Management", "733101" },
+                    { 3, "Kenchenhalli", "/images/ab.jpg", "Bangalore", "India", "paulab@gmail.com", "AB PAUL", "+91 12333567890", "Trainer and Consultant", "560098" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "RoleDescription", "RoleName" },
+                values: new object[,]
+                {
+                    { 1, "Employee of Organisation", "Employee" },
+                    { 2, "HR of Organisation", "HR" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
