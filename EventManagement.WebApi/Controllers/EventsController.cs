264fc529c6989a9da266d9b25d51df4d9e7ba197
Akash Paul <paulakash36@outlook.com>
2023-02-04 01:17:32 +0530

    Adding Events Controller

---
+++ EventManagement.WebApi/Controllers/EventsController.cs
@@ -0,0 +1,77 @@
+﻿using EventManagement.Dal;
+using EventManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagement.WebApi.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ICommonRepository<Event> _repository;
+        public EventsController(ICommonRepository<Event> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<Event>> Get()
+        {
+            var events = _repository.GetAll();
+            if (events.Count == 0)
+                return NotFound();
+            return Ok(events);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<Event>> GetDetails(int id)
+        {
+            var eventItem = _repository.GetDetails(id);
+            return eventItem == null ? NotFound() : Ok(eventItem);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult Create(Event eventItem)
+        {
+            _repository.Insert(eventItem);
+            var result = _repository.SaveChanges();
+            if (result > 0)
+                return CreatedAtAction("GetDetails", new { id = eventItem.Eventid }, eventItem);
+            return BadRequest();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public ActionResult Update(Event eventItem)
+        {
+            _repository.Update(eventItem);
+            var result = _repository.SaveChanges();
+            if (result > 0)
+                return NoContent();
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public ActionResult<Event> Delete(int id)
+        {
+            var eventItem = _repository.GetDetails(id);
+            if (eventItem == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _repository.Delete(eventItem);
+                _repository.SaveChanges();
+                return NoContent();
+            }
+        }
+    }
+}
--- EventManagement.WebApi/Program.cs
+++ EventManagement.WebApi/Program.cs
@@ -16,6 +16,8 @@ namespace EventManagement.WebApi
 
             builder.Services.AddTransient<ICommonRepository<Employee>, CommonRepository<Employee>>();
 
+            builder.Services.AddTransient<ICommonRepository<Event>, CommonRepository<Event>>();
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
